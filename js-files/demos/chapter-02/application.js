acc0eb5d9f22a3a0883cdcb8dcb06cc8140198d7
lucio-cornejo <luciocornejo11@gmailcom>
2023-03-02 09:16:52 -0500

    add module demo for chapter 2

---
+++ js-files/demos/chapter-02/application.js
@@ -0,0 +1,6 @@
+const utilities = require('./utilities');
+
+const data = [-1, 5, 3, 0, 10];
+
+console.log(`clip([${data}]) -> ${utilities.clip(data)}`);
+console.log(`clip([${data}], 5) -> ${utilities.clip(data, 5)}`);
