6c38e3263404dc5eb45dd91b6f648f82770fbb84
Lucio Cornejo <85001476+lucio-cornejo@users.noreply.github.com>
2023-03-02 09:52:29 -0500

    Add notes and files for chapter 02 (#4)
    * create folder for js examples from the book
    
    * add folder for images
    
    * add notes for chapter 2
    
    * add module demo for chapter 2
    
    * include exercises in the main book
    
    ---------
    
    Co-authored-by: lucio-cornejo <luciocornejo11@gmailcom>

---
+++ js-files/chapter-02/utilities.js
@@ -0,0 +1,15 @@
+DEFAULT_BOUND = 3
+
+const clip = (values, bound = DEFAULT_BOUND) => {
+  let result = [];
+  for (let v of values) {
+    if (v <= bound) {
+      result.push(v);
+    }
+  }
+  return result;
+}
+
+module.exports = {
+  clip: clip
+}
